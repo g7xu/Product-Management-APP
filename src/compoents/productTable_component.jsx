2664d1bb4788284c4a02b13ba4c3dc09a5fde098
Guoxuan Xu <guoxuan.xu8@gmail.com>
2025-11-13 22:26:33 -0800

    Update productTable_component.jsx

--- src/compoents/productTable_component.jsx
+++ src/compoents/productTable_component.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 
-export default class ClassComponent extends Component {
-  constructor(prop) {
-    super(prop);
-    this.state = { ...prop };
+export default class ProductTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products : this.props.init_table_prop
+    };
   }
 
   render() {
-    return;
+    return <div>
+      {/* {Product Section} */}
+      <div>
+        <input type="text" placeholder="Product Name" />
+        <button>Add Product</button>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Product ID</th>
+            <th>Product Name</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.name}</td>
+              <td>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      
+    </div>
   }
 }
